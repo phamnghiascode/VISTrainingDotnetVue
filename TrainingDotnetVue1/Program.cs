01727b99609c4815e24612a5a52c9a684c5e8adc
DC-VIETIS\NghiaPT <nghiapt@vietis.com.vn>
2025-09-12 09:39:57 +0700

    Add project files.

---
+++ TrainingDotnetVue1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TrainingDotnetVue1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
